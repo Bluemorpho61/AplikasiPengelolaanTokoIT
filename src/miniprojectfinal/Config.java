5ee24bb3a1e53d927af48086bf88c0795f614599
Bluemorpho61 <vanda10.dava@gmail.com>
2021-11-29 06:09:00 +0700

    Project TokoIT

---
+++ src/miniprojectfinal/Config.java
@@ -0,0 +1,34 @@
+package miniprojectfinal;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class Config {
+    private static Connection mysqlconfig;
+    public static Connection configDB()throws SQLException{
+        try { 
+            String url="jdbc:mysql://localhost:3306/toko_barang_it"; //url database
+            String user="root"; //user database
+            String pass=" "; //password database
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+        } catch (Exception e) {
+            System.err.println("Koneksi gagal "+e.getMessage());
+            // perintah menampilkan error [ada koneksi
+        }
+        return mysqlconfig;
+}
+
+}
---
+++ src/miniprojectfinal/Koneksi/Konek.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miniprojectfinal.Koneksi;
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author Alkin PC
+ */
+public class Konek {
+    private static Connection koneksi;
+    public static Connection getConnection() throws SQLException{
+        if (koneksi==null) {
+            new Driver();
+            
+            koneksi=DriverManager.getConnection("jdbc:mysql://localhost:3306/toko_barang_it","root","");
+        }
+        return koneksi;
+}
+}
