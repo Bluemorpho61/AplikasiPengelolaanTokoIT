248b7845b851741489244d4217e5f0bdb7dcb4e8
Bluemorpho61 <74475354+Bluemorpho61@users.noreply.github.com>
2021-12-08 20:15:30 +0700

    Add files via upload

---
+++ toko_barang_it_lama .sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 08, 2021 at 01:37 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.4.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_barang_it_lama`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_akun`
+--
+
+CREATE TABLE `tb_akun` (
+  `username` varchar(15) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `nama_jenispengguna` char(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_akun`
+--
+
+INSERT INTO `tb_akun` (`username`, `password`, `nama_jenispengguna`) VALUES
+('admin', 'admin', 'owner'),
+('Budi', '123', 'Karyawan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `jenis` varchar(20) NOT NULL,
+  `tipe_model` varchar(10) NOT NULL,
+  `Brand_barang` longtext NOT NULL,
+  `supplier` varchar(10) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `Tgl_masuk` date NOT NULL,
+  `jumlah` char(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `nama`, `jenis`, `tipe_model`, `Brand_barang`, `supplier`, `harga`, `Tgl_masuk`, `jumlah`) VALUES
+(777, 'Rexus GX7', 'Aksessoris PC', 'GX7', 'Rexus', '345', 700000, '2021-12-14', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `alamat` longtext NOT NULL,
+  `jenis_kelamin` longtext NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `tanggal_masuk` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`id_karyawan`, `nama`, `alamat`, `jenis_kelamin`, `tanggal_lahir`, `tanggal_masuk`) VALUES
+(222, 'Budi', 'Jember', 'Pria', '1999-05-20', '2021-05-13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_supplier`
+--
+
+CREATE TABLE `tb_supplier` (
+  `id_supplier` varchar(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `alamat` varchar(20) NOT NULL,
+  `no_telp` char(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_supplier`
+--
+
+INSERT INTO `tb_supplier` (`id_supplier`, `nama`, `alamat`, `no_telp`) VALUES
+('345', 'PT Jaya Abadi', 'Jember', '08765324421');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `no_transaksi` varchar(10) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `tipe_barang` varchar(20) NOT NULL,
+  `merek_barang` char(15) NOT NULL,
+  `harga` varchar(15) NOT NULL,
+  `jumlah` char(3) NOT NULL,
+  `total` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `tb_barang` AFTER INSERT ON `transaksi` FOR EACH ROW BEGIN
+UPDATE tb_barang
+SET jumlah = jumlah-NEW.jumlah
+WHERE
+id_barang =NEW.id_barang;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `supplier` (`supplier`);
+
+--
+-- Indexes for table `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indexes for table `tb_supplier`
+--
+ALTER TABLE `tb_supplier`
+  ADD PRIMARY KEY (`id_supplier`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`no_transaksi`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD CONSTRAINT `tb_barang_ibfk_1` FOREIGN KEY (`supplier`) REFERENCES `tb_supplier` (`id_supplier`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
